9e12cd726ac255acfdde347e65f8a765ed39f9c2
20181150 <78853666+20181150@users.noreply.github.com>
2021-03-04 19:23:07 +0900

    Add files via upload

---
+++ content/js/toggle.js
@@ -0,0 +1,75 @@
+function setEditEnable1() {
+  if (document.getElementById("1").checked) {
+    mainDetails1.open = false;
+  } else {
+    mainDetails1.open = true;
+  }
+}
+function setCheckbox1() {
+  if (mainDetails1.open) {
+    document.getElementById("1").checked = true;
+  } else {
+    document.getElementById("1").checked = false;
+  }
+}
+
+function setEditEnable2() {
+  if (document.getElementById("2").checked) {
+    mainDetails2.open = false;
+  } else {
+    mainDetails2.open = true;
+  }
+}
+function setCheckbox2() {
+  if (mainDetails2.open) {
+    document.getElementById("2").checked = true;
+  } else {
+    document.getElementById("2").checked = false;
+  }
+}
+
+function setEditEnable3() {
+  if (document.getElementById("3").checked) {
+    mainDetails3.open = false;
+  } else {
+    mainDetails3.open = true;
+  }
+}
+function setCheckbox3() {
+  if (mainDetails3.open) {
+    document.getElementById("3").checked = true;
+  } else {
+    document.getElementById("3").checked = false;
+  }
+}
+
+function setEditEnable4() {
+  if (document.getElementById("4").checked) {
+    mainDetails4.open = false;
+  } else {
+    mainDetails4.open = true;
+  }
+}
+function setCheckbox4() {
+  if (mainDetails4.open) {
+    document.getElementById("4").checked = true;
+  } else {
+    document.getElementById("4").checked = false;
+  }
+}
+/*
+function setEditEnable5() {
+  if (document.getElementById("5").checked) {
+    mainDetails5.open = false;
+  } else {
+    mainDetails5.open = true;
+  }
+}
+function setCheckbox5() {
+  if (mainDetails5.open) {
+    document.getElementById("5").checked = true;
+  } else {
+    document.getElementById("5").checked = false;
+  }
+}
+*/
